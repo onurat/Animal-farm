201b123be08c6ce2c2f13a755267fdc7f47d3a74
onurat <114289826+onurat@users.noreply.github.com>
2024-02-05 19:14:12 +0000

    .

--- booking-app-backend/booking-app-backend/server.js
+++ booking-app-backend/booking-app-backend/server.js
@@ -5,9 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-
 const pool = new Pool({
-  connectionString: 'postgres://fvjsfzhl:TmP6jGcrNCVgN5Nh6hhJOjYvWt87RItO@peanut.db.elephantsql.com/fvjsfzhl',
+  connectionString: process.env.DATABASE_URL,
 });
 
 app.use(express.json());
@@ -17,21 +16,21 @@ app.listen(port, () => {
 });
 
 app.post('/api/bookings', async (req, res) => {
-    const { name, phone, email, selectedDate } = req.body;
-  
-    try {
-      const client = await pool.connect();
-      await client.query('INSERT INTO bookings (name, phone, email, selected_date) VALUES ($1, $2, $3, $4)', [
-        name,
-        phone,
-        email,
-        selectedDate,
-      ]);
-      client.release();
-  
-      res.status(201).json({ message: 'Booking created successfully' });
-    } catch (error) {
-      console.error('Error creating booking:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+  const { name, phone, email, selectedDate } = req.body;
+
+  try {
+    const client = await pool.connect();
+    await client.query('INSERT INTO bookings (name, phone, email, selected_date) VALUES ($1, $2, $3, $4)', [
+      name,
+      phone,
+      email,
+      selectedDate,
+    ]);
+    client.release();
+
+    res.status(201).json({ message: 'Booking created successfully' });
+  } catch (error) {
+    console.error('Error creating booking:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
